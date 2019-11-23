e270f6057dcbd0759fb712af476db1745816510e
lmvasquezg <lmvasquezg@eafit.edu.co>
2019-11-23 01:13:38 -0500

    Odio al vida

--- src/sisctrl.cpp
+++ src/sisctrl.cpp
@@ -1,114 +1,104 @@
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <iostream>
-#include <bits/stdc++.h> 
- 
+       #include <sys/wait.h>
+       #include <stdio.h>
+       #include <stdlib.h>
+       #include <unistd.h>
+       #include <string.h>  
+       #include <iostream>
 
+       using namespace std;
 
-int main(int argc, char *argv[])
-{    
-    int pipei[2]; // Pipe from parent to child for input
-    int pipes[2];  //Pipe from child to child for processing
-    int pipeo[2];  //Pipe from child to parent for error
-    pid_t cpid;
-    char buf;
-    
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    if (pipe(pipes) == -1 || pipe(pipei) == -1 || pipe(pipeo) == -1  ) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-    
-    cpid=fork();
-    if (cpid== -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    
-    
-    if (cpid == 0) {         // Si soy el hijo
-      
-        close(pipei[1]);      //Cierro lado de escritura del input
-        
+       int
+       main(int argc, char *argv[])
+       {
+           int pipefd[2];
+           int pipef[2];
+           int pipes[2];
+           pid_t cpid;
+           char buf;
+           
+           if (pipe(pipefd) == -1 || pipe(pipef)==-1 || pipe(pipes)==-1) {
+               perror("pipe");
+               exit(EXIT_FAILURE);
+           }
+           
+           cpid = fork();
+           if (cpid == -1) {
+               perror("fork");
+               exit(EXIT_FAILURE);
+           }  
+              
+           if (cpid == 0) { 
+               close(pipefd[1]);   //Child process
+               close(pipef[0]);
+               std::string chain ="";
+               char buf;
+               while (read(pipefd[0], &buf, 1) > 0){  //Leo caracter por caracter de input (porque no hay hilos)
+                std::string s(1, buf);
+                chain.append(s);
+               }
+           
+                printf("Hello");
+                if(chain.length()==1){
+                    if(chain.find('a') == 0){
+                     close(pipefd[0]);
+                     std::string s ="R";
+                     write(pipef[1],s.c_str(),1);
+                       close(pipef[1]);
+                       
+                     _exit(EXIT_SUCCESS);
+                    }else{
+                        close(pipefd[0]);
+                     std::string s ="D";
+                     write(pipef[1],s.c_str(),1);
+                       close(pipef[1]);
+                     _exit(EXIT_SUCCESS);
+                    }
+                    
+                }else{
+                    
+                    char c = chain.at(0);
+                    
+                    cout<<c;
+                    _exit(EXIT_SUCCESS);
+                    //const char* cha = chain.substr(1).c_str();
+                    char* cha ="ju";
+                    if(c=='a'){
+                        
+                        //write(pipes[1],&cha,3);
+                        
+                    }else{
+                        close(pipefd[0]);
+                     std::string s ="D";
+                     write(pipef[1],s.c_str(),1);
+                       close(pipef[1]);
+                     _exit(EXIT_SUCCESS);
+                    }
+                    
+                }
+                chain ="";
                 
-
-        char buf;
-        close(pipeo[0]);       // Close read from output
-        std::string chain ="";   // Mensaje
-        while(true){
-        while (read(pipei[0], &buf, 1) > 0){  //Leo caracter por caracter de input (porque no hay hilos)
-            std::string s(1, buf);
-            chain.append(s);
-        }
-        if(chain.length()>0){
-            break;
-        }
-        }
-        
-       /*  while(true){  
-        while (read(pipes[0], &buf, 1) > 0){ //Leo caracter por caracter de la tuberia a mi mismo (porque no hay hilos)
-            std::string s(1, buf);
-            chain.append(s);
-        } */
-         
-        char c = chain.at(0);   // Cojo el primer caracter
-        
-        if(c=='a'){
-            chain =chain.substr(1);   // Lo elimino de la cadena 
-            if(chain.length()==0){    // Si no queda nada la cadena se reconoce
-                std::string r="R";
-                write(pipeo[1],r.c_str(),1);  //Escribo en output 
-                exit(EXIT_SUCCESS);
-            }
-           // write(pipes[1],chain.c_str(),chain.length()-1); //Sino, solo lo mando otra vez a la tuberia de si mismo
-        }else{
-            std::string nr="NR";           //Si no es a, no pasa y no es reconocido
-            write(pipeo[1],nr.c_str(),2);
-            close(pipes[0]);
-            exit(EXIT_SUCCESS);
-        }
-        printf("Hsat aca hijo");
-         exit(EXIT_SUCCESS);
-
-    } else {   
-                 
-        close(pipei[0]);      //Cierro lectura de tuberia
-        close(pipeo[1]);     // Cierro escritura del output
-        close(pipes[0]);     // Cierro Tuberia que es solo del hijo
-        close(pipes[1]);
-       
-        write(pipei[1], argv[1], strlen(argv[1]));  // Escribo cadena en input
-       
-        char bufp;
-           // Close read from output
-        std::string chainp ="";   // Mensaje
-        
-        wait(NULL);
+                while (read(pipes[0], &buf, 1) > 0){  //Leo caracter por caracter de input (porque no hay hilos)
+                    std::string s(1, buf);
+                    chain.append(s);
+                }
+                printf(chain.c_str());
            
-        while(read(pipeo[0], &bufp, 1) > 0){  //Leo caracter por caracter de input (porque no hay hilos)
-           
-            std::string sp(1, buf);
-            chainp.append(sp);
-        }
-         /* Reader will see EOF */
-        //wait(&cpid);   
-        
-        printf("Hsat aca padre");             /* Wait for child */
-        exit(EXIT_SUCCESS);
-    }
-}
-
-
-
-
-
-    
-
+           }
+           else {  //Parent process
+               close(pipefd[0]);
+               close(pipef[1]);
+               close(pipes[0]);
+               close(pipes[1]);
+               char buf;
+               write(pipefd[1],argv[1] , strlen(argv[1]));
+               close(pipefd[1]);
+               //read(pipef[0], &buf, 1) ;
+                   cout << "\nExiting program..."<<buf;
+                   close(pipef[0]);
+                   int status;
+                   waitpid(cpid, &status, 0);
+                   exit(EXIT_SUCCESS);
+               
+           }  
+       }
