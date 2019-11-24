4eba4e5cf3f851f2701387b84f216dcc3759185b
imurielr <imurielr@eafit.edu.co>
2019-11-23 20:29:26 -0500

    Hasta cierre de finales

--- makefile
+++ makefile
@@ -1,9 +1,20 @@
-all: sisctrl
+all: src/sisctrl
 
-sisctrl: src/sisctrl.o 
-	$(CXX) -o $@ $^
+CXXFLAGS=-g
+LDFLAGS=-L /usr/local/lib -lyaml-cpp
+
+# INVOICE_OBJECTS=src/sisctrl.o src/automata.o
+INVOICE_OBJECTS=src/sisctrl.o
 
 
+src/sisctrl: $(INVOICE_OBJECTS)
+	$(CXX) -o $@ $^  $(LDFLAGS)
+
+src/sisctrl.o: src/sisctrl.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+# src/automata.o: src/automata.cpp src/automata.h
+# 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 clean:
-	rm src/*.o
+	rm -f src/*.o src/*.cpp~ Makefile~
--- src/sisctrl.cpp
+++ src/sisctrl.cpp
@@ -1,26 +1,156 @@
 #include <sys/types.h>
-       #include <sys/wait.h>
-       #include <stdio.h>
-       #include <stdlib.h>
-       #include <unistd.h>
-       #include <string.h>  
-       #include <iostream>
-        #include <vector>
-        #include <yaml-cpp/yaml.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <yaml-cpp/yaml.h>
+#include <vector>
+
+using namespace std;
+
+struct arista {
+    string nodeIn;
+    string nodeOut;
+    string trans;
+    int pipefd[2];
+};
+
+void arista_tostring(arista edge);
+
+void closeFinals(map<string,map<string, int[2]>> &finals, string &name, string &autom);
+
+void createChild(YAML::Node& doc,map <string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error);
+
+void closeUnusedPipes(string &autom,string &name, map <string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error);
+
+void createPipe(int* p){
+      if(pipe(p)==-1){
+          //Error creando tuberia
+          exit(EXIT_FAILURE);
+      }
+}
 
 
-   using namespace std;
-       int
-       main(int argc, char *argv[])
-       {    
-           YAML::Node doc;
-           try {
-                doc = YAML::LoadFile("datos.yaml");
-           } catch (exception& e) {
-               cout << "El error es " << e.what() << endl;
-           }
-            
-           cout<<doc[0]["invoice"].as<std::string>() <<endl;;
-            return 0;
 
-       }
+map <string, vector<arista>> grafo;
+
+
+int main(int argc, char *argv[]) {
+    YAML::Node doc;
+
+
+    try {
+        doc = YAML::LoadFile(argv[1]);
+    }
+    catch (exception &e) {
+        cout << "El error es " << e.what() << endl;
+    }
+
+    arista edge;
+    map<string, map<string, int[2]>> error, finals, initial;
+    // vector<string> states;
+    for (unsigned int i = 0; i < doc.size(); i++) { // Ciclo por cada automata
+        YAML::Node delta = doc[i]["delta"];
+        vector<arista> aristas;
+        string nomAutomata = doc[i]["automata"].as<string>();
+        createPipe(initial[nomAutomata][doc[i]["start"].as<string>()]);
+
+        for (int f = 0; f < doc[i]["final"].size(); f++) {            
+            createPipe(finals[nomAutomata][doc[i]["final"][f].as<string>()]);
+        }
+
+
+        for (unsigned int j = 0; j < delta.size(); j++) {
+            YAML::Node trans = delta[j]["trans"];
+
+            createPipe(error[nomAutomata][delta[j]["node"].as<string>()]);
+
+            for (unsigned int k = 0; k < trans.size(); k++) {
+                edge.nodeIn = delta[j]["node"].as<string>();
+                edge.nodeOut = trans[k]["next"].as<string>();
+                edge.trans = trans[k]["in"].as<string>();
+                createPipe(edge.pipefd);
+                aristas.push_back(edge);
+                grafo[nomAutomata] = aristas;
+
+                // arista_tostring(grafo[doc[i]["automata"].as<string>()][k]);
+                // cout << "Automata: " << nomAutomata << "   " << "Estado: " << states[nomAutomata][k] << endl;
+            }
+        }
+    }
+    createChild(doc,grafo,finals,initial,error);
+    //     cout << "----------------------------" << endl;
+
+    // for (auto const& x : states) {
+    //     for (auto const& s : x.second) {
+    //         cout << "Automata: " << x.first << "   " << endl;
+    //     }
+    // }
+
+    // for (auto const& x : grafo) {
+    //     for (unsigned int i = 0; i < x.second.size(); i++) {
+    //         arista_tostring(grafo[x.first][i]);
+    //     }
+    // }
+
+    return 0;
+}
+
+void createChild(YAML::Node& doc,map <string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error) {
+    int cpid;
+    vector<int> pids;
+    for (unsigned int i = 0; i < doc.size(); i++) {
+        string automata = doc[i]["automata"].as<string>();
+        YAML::Node delta = doc[i]["delta"];
+        for (unsigned j = 0; j < delta.size(); j++) {
+            string node = delta[j]["node"].as<string>();
+            if ((cpid = fork()) == 0) {
+                closeUnusedPipes(automata,node,graph,finals,initial,error);
+                exit(EXIT_SUCCESS);
+            }
+            pids.push_back(cpid);
+        }
+    }
+    int status;
+    for (int id : pids) {
+        waitpid(id, &status, 0);
+    }
+    cout << "Mis hijos murieron, me voy a suicidar" << endl;
+    exit(EXIT_SUCCESS);
+}
+
+void closeUnusedPipes(string &autom,string &name, map <string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error) {
+    closeFinals(finals,name,autom);
+
+
+}
+
+void closeFinals(map<string,map<string, int[2]>> &finals, string &name, string &autom){
+    for(auto const& mapa: finals){
+        for(auto const& p: mapa.second){
+            if(p.first==name && mapa.first==autom){
+                cout << name << " is final in " << autom << endl;
+                close(p.second[0]);
+            }else{
+                close(p.second[0]);
+                close(p.second[1]);
+            }
+
+        }
+    }
+}
+void closeInitial(map<string,map<string, int[2]>> &initial, string &name){}
+void closeError(map<string,map<string, int[2]>> &error, string &name){}
+void closeEdges(map<string,vector<arista>> &graph, string &name){}
+
+
+
+
+void arista_tostring(arista edge) {
+    cout << "Node In: " << edge.nodeIn << endl;
+    cout << "Trans: " << edge.trans << endl;
+    cout << "Node Out: " << edge.nodeOut << endl;
+}
