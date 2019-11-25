c104d60e28af4d17b0c00e03812729e374cb53aa
lmvasquezg <lmvasquezg@eafit.edu.co>
2019-11-25 04:34:10 -0500

    Probando superioridad femenina

--- src/sisctrl.cpp
+++ src/sisctrl.cpp
@@ -11,6 +11,7 @@
 #include <thread>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <signal.h>
 
 using namespace std;
 
@@ -30,7 +31,11 @@ void closeEdges(map<string,vector<arista>> &graph, string &name,string &autom ,b
 void threadIteration(string autom, string name ,int pipe);
 void finalRead(int pipe, string autom);
 void errRead(int pipe, string autom);
+void manejoCC(int n);
 void sendRecognized(string recog, string rest, string autom, string name);
+void sendUnrecognized(string recog, string rest, string autom, string name);
+void sendToNextPipe(string recog, string rest, string autom, string name);
+bool isPrefix(string prefix, string full);
 
 void createChild(YAML::Node& doc,map <string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error);
 void closePipesSisctrl(map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error, map <string, vector<arista>> &graph, bool &t);
@@ -42,10 +47,12 @@ void createPipe(int* p);
 sem_t mutex1, mutex2, mutex3, mutex4; //mutex3: State error, mutex4: final State, mutex2:cout ssisctrl
 map <string, vector<arista>> grafo;
 map<string, map<string, int[2]>> error, finals, initial;
+map<string,map<string,int>> pids;
 
 int main(int argc, char *argv[]) {
     YAML::Node doc;
-
+    sighandler_t ant;
+    ant =signal(SIGINT,manejoCC);
     if (argc != 2) {
         cerr << "Usage: ./sisctrl <file.yaml>" << endl;
         exit(EXIT_FAILURE);
@@ -99,18 +106,34 @@ void createPipe(int* p){
           exit(EXIT_FAILURE);
       }
       if(fcntl(p[0],F_SETFL,O_NONBLOCK)<0){
-          cout<< "ey" << endl;
+          
           exit(EXIT_FAILURE);
       }
       if(fcntl(p[1],F_SETFL,O_NONBLOCK)<0){
-          cout<< "ey" << endl;
+          
           exit(EXIT_FAILURE);
       }
 }
 
+void manejoCC(int n){
+    for(auto const& map : pids){
+        for (auto const& p: map.second) {
+                kill(p.second,SIGKILL);
+        }
+    }
+    //cout << "La auto moricion" << endl;
+    exit(EXIT_SUCCESS);
+
+}
+
+bool isPrefix(string prefix, string full)
+{
+    return prefix == full.substr(0, prefix.size());
+}
+
 void createChild(YAML::Node& doc,map<string, vector<arista>> &graph, map<string, map<string, int[2]>> &finals, map<string, map<string, int[2]>> &initial, map<string, map<string, int[2]>> &error) {
     int cpid;
-    map<string,map<string,int>> pids;
+    //map<string,map<string,int>> pids;
     for (unsigned int i = 0; i < doc.size(); i++) {
         string automata = doc[i]["automata"].as<string>();
         YAML::Node delta = doc[i]["delta"];
@@ -122,10 +145,8 @@ void createChild(YAML::Node& doc,map<string, vector<arista>> &graph, map<string,
                 sem_init(&mutex3,0,1);
                 sem_init(&mutex4,0,1);
                 createThreads(automata,node,graph, finals,initial, error);
-                //
-                //
-                //
-                cout << "Pos me mato" << endl;
+
+                //cout << "Pos me mato" << endl;
                 ps=true;
                 //closeUnusedPipes(automata,node,graph,finals,initial,error,ps);
                 
@@ -302,7 +323,7 @@ void threadIteration(string autom, string name,int pipe){
         char buf;
         string msg ="";
         string recog,rest;
-        while((read(pipe,&buf,1)>0)&&(buf!='}')){
+        while((read(pipe,&buf,1)>0)){
             msg.push_back(buf);
         }
         if(msg.length()>0){
@@ -311,18 +332,16 @@ void threadIteration(string autom, string name,int pipe){
             rest = doc["rest"].as<string>();
             if(rest.length()==0){
                 if(finals[autom].count(name)>0){
-                    cout << "reconoci" << endl;
-                    // sendRecognized(recog,rest,autom,name);
+                    sendRecognized(recog,rest,autom,name);
                 }else{
-                    cout << "ey que el pasa loko" << endl;
+                    sendUnrecognized(recog,rest,autom,name);
                 }
+            }else{
+                sendToNextPipe(recog,rest,autom,name);
             }
         }
     }
-        
-        
-    
-     
+
     
 }
 
@@ -338,62 +357,144 @@ void sendRecognized(string recog, string rest, string autom, string name){
     out<<YAML::Key << "rest";
     out<<YAML::Value << rest;
     out<<YAML::EndMap;
-    cout << out.c_str()<< endl;
+    
     const char* str = out.c_str();
+    sem_wait(&mutex3);
+     write(pipe,out.c_str(),strlen(str));
+         
+    sem_post(&mutex3);
+
+}
+
+void sendPipeError(string recog, string rest,string autom, string name){
+    int pipe = error[autom][name][1];
+    YAML::Emitter out;
+    out << YAML::Flow;
+    out << YAML::BeginMap;
+    out<<YAML::Key << "codterm";
+    out<<YAML::Value << "2";
+    out<<YAML::Key << "recog";
+    out<<YAML::Value << recog;
+    out<<YAML::Key << "rest";
+    out<<YAML::Value << rest;
+    out<<YAML::EndMap;
+    
+    const char* str = out.c_str();
+    sem_wait(&mutex4);
     write(pipe,out.c_str(),strlen(str));
+    sem_post(&mutex4);
+
+}
+
+void sendUnrecognized(string recog, string rest, string autom, string name){
+    int pipe = error[autom][name][1];
+    YAML::Emitter out;
+    out << YAML::Flow;
+    out << YAML::BeginMap;
+    out<<YAML::Key << "codterm";
+    out<<YAML::Value << "1";
+    out<<YAML::Key << "recog";
+    out<<YAML::Value << recog;
+    out<<YAML::Key << "rest";
+    out<<YAML::Value << rest;
+    out<<YAML::EndMap;
+    
+    const char* str = out.c_str();
+    sem_wait(&mutex4);
+    write(pipe,out.c_str(),strlen(str));
+    sem_post(&mutex4);
+
+}
+
+void sendToNextPipe(string recog, string rest, string autom, string name){
+    bool validt =false;
+    for(arista ar : grafo[autom]){
+        if(ar.nodeIn==name && isPrefix(ar.trans, rest)){
+            validt=true;
+            string nrest = rest.substr(ar.trans.size(),rest.size());
+            string nrecog = recog+ar.trans;
+            YAML::Emitter out;
+            out << YAML::Flow;
+            out << YAML::BeginMap;
+            out << YAML::Key << "recog";
+            out << YAML::Value << nrecog;
+            out << YAML::Key << "rest";
+            out << YAML::Value << nrest;
+            out << YAML::EndMap;
+
+            const char* str = out.c_str();
+            sem_wait(&mutex3);
+            if(write(ar.pipefd[1],str,strlen(str))<0){
+                string str = "Pid: "+getpid();
+                sendPipeError(str,"broken pipe on function sndMsg", autom,name);
+            }
+            sem_post(&mutex3);
+           
+        }
+    }
+    if(!validt){
+        sendUnrecognized(recog,rest,autom,name);
+    }
 
 }
 
 
 
 void errRead(int pipe, string autom){
-    //read(pipe,buf,1)
-    string msg="{ codterm: 2 , recog: Pid:1208 , rest: \"broken pipe on function sndMsg\" }";
-    string codterm,recog,rest;
-    YAML::Node doc = YAML::Load(msg);
-    codterm = doc["codterm"].as<string>();
-    recog = doc["recog"].as<string>();
-    rest = doc["rest"].as<string>();
-    YAML::Emitter out;
-    if(codterm=="1"){
-        
-        out << YAML::BeginSeq << YAML::BeginMap;
-        out << YAML::Key <<"msgtype";
-        out << YAML::Value <<"reject";
-        out << YAML::Key << "reject";
-        out << YAML::Value << YAML::BeginSeq << YAML::BeginMap;
-        out << YAML::Key <<"automata";
-        out << YAML::Value << autom;
-        out << YAML::Key <<"msg";
-        out << YAML::Value <<recog+rest;
-        out << YAML::Key <<"pos";
-        out << YAML::Value <<recog.length();
-        out << YAML::EndMap;
-        out << YAML::EndSeq;
-        out << YAML::EndMap;
-        out << YAML::EndSeq;
-        sem_wait(&mutex2);
-        //cout << out.c_str()<< endl; 
-        sem_post(&mutex2);
+    while(true){
+        string msg="";
+        char buf;
+        while(read(pipe,&buf,1)>0){
+            msg.push_back(buf);
+        }
+        if(msg.length()!=0){
+            string codterm,recog,rest;
+            YAML::Node doc = YAML::Load(msg);
+            codterm = doc["codterm"].as<string>();
+            recog = doc["recog"].as<string>();
+            rest = doc["rest"].as<string>();
+            YAML::Emitter out;
+            if(codterm=="1"){
+                
+                out << YAML::BeginSeq << YAML::BeginMap;
+                out << YAML::Key <<"msgtype";
+                out << YAML::Value <<"reject";
+                out << YAML::Key << "reject";
+                out << YAML::Value << YAML::BeginSeq << YAML::BeginMap;
+                out << YAML::Key <<"automata";
+                out << YAML::Value << autom;
+                out << YAML::Key <<"msg";
+                out << YAML::Value <<recog+rest;
+                out << YAML::Key <<"pos";
+                out << YAML::Value <<recog.length();
+                out << YAML::EndMap;
+                out << YAML::EndSeq;
+                out << YAML::EndMap;
+                out << YAML::EndSeq;
+                sem_wait(&mutex2);
+                cout << out.c_str()<< endl; 
+                sem_post(&mutex2);
 
-    }else{
-        
-        out << YAML::BeginSeq << YAML::BeginMap;
-        out << YAML::Key <<"msgtype";
-        out << YAML::Value <<"error";
-        out << YAML::Key << "error";
-        out << YAML::Value << YAML::BeginSeq << YAML::BeginMap;
-        out << YAML::Key <<"where";
-        out << YAML::Value << recog;
-        out << YAML::Key <<"cause";
-        out << YAML::Value <<rest;
-        out << YAML::EndMap;
-        out << YAML::EndSeq;
-        out << YAML::EndMap;
-        out << YAML::EndSeq;
-        sem_wait(&mutex2);
-       // cout << out.c_str()<< endl; 
-        sem_post(&mutex2);
+            }else{
+                
+                out << YAML::BeginSeq << YAML::BeginMap;
+                out << YAML::Key <<"msgtype";
+                out << YAML::Value <<"error";
+                out << YAML::Key << "error";
+                out << YAML::Value << YAML::BeginSeq << YAML::BeginMap;
+                out << YAML::Key <<"where";
+                out << YAML::Value << recog;
+                out << YAML::Key <<"cause";
+                out << YAML::Value <<rest;
+                out << YAML::EndMap;
+                out << YAML::EndSeq;
+                out << YAML::EndMap;
+                out << YAML::EndSeq;
+                sem_wait(&mutex2);
+                cout << out.c_str()<< endl; 
+                sem_post(&mutex2);
+            }
+        }
     }
 
 }
@@ -411,6 +512,7 @@ void finalRead(int pipe, string autom){
             codterm = doc["codterm"].as<string>();
             recog = doc["recog"].as<string>();
             rest = doc["rest"].as<string>();
+            
             YAML::Emitter out;
             out << YAML::BeginSeq << YAML::BeginMap;
             out << YAML::Key <<"msgtype";
@@ -475,13 +577,16 @@ void inOutOperations(map<string,map<string,int>> pids, map<string,map<string, in
                     out<<YAML::Key << "rest";
                     out<<YAML::Value << msg;
                     out<<YAML::EndMap;
-                    //cout<< out.c_str()<< endl;
+                    
+                    
                     const char* str = out.c_str();
+                    //cout << str << endl;
                     write(p.second[1],str,strlen(str));
                         
                 }
             }
         }else if(cmd=="info"){
+            
             YAML::Emitter out;
             out << YAML::BeginSeq<<YAML::BeginMap;
             out <<YAML::Key << "msgtype";
@@ -504,15 +609,17 @@ void inOutOperations(map<string,map<string,int>> pids, map<string,map<string, in
                         out << YAML::Value << p.first;
                         out << YAML::Key << "pid";
                         out << YAML::Value << p.second;
-                        
                         out << YAML::EndMap;
                     }
                     out << YAML::EndSeq;
-                    out << YAML::EndMap;    
-                    sem_wait(&mutex2);
-                    cout << out.c_str() << endl;
-                    sem_post(&mutex2);
-                }
+                    out << YAML::EndMap;
+                }  
+                out << YAML::EndSeq;
+                out << YAML::EndMap;
+                sem_wait(&mutex2);
+                cout << out.c_str() << endl;
+                sem_post(&mutex2);
+                
             }else{
                 if(pids.count(msg)>0){
                     out << YAML::BeginMap;
@@ -532,6 +639,8 @@ void inOutOperations(map<string,map<string,int>> pids, map<string,map<string, in
                         out << YAML::EndMap;
                     }
                     out << YAML::EndSeq;
+                    out << YAML::EndMap; 
+                    out << YAML::EndSeq;
                     out << YAML::EndMap;    
                     sem_wait(&mutex2);
                     cout << out.c_str() << endl;
@@ -545,22 +654,20 @@ void inOutOperations(map<string,map<string,int>> pids, map<string,map<string, in
                 }
 
             }
-            }else if(cmd=="stop"){
+        }else if(cmd=="stop"){
                 
-                for(auto const& map : pids){
-                    for (auto const& p: map.second) {
-                         kill(p.second,SIGKILL);
-                    }
+            for(auto const& map : pids){
+                for (auto const& p: map.second) {
+                        kill(p.second,SIGKILL);
                 }
-                
-                
-                break;
-            }else{
-                sem_wait(&mutex2);
-                cout << "Please enter a valid command" << endl;
-                sem_post(&mutex2);
             }
+            break;
+        }else{
+            sem_wait(&mutex2);
+            cout << "Please enter a valid command" << endl;
+            sem_post(&mutex2);
         }
+    }
 
 
     int status;
